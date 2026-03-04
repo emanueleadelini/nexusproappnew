d604d023f683c6e3d6a24590475397ea8d92583a
emanueleadelini <emanueleadelini@gmail.com>
2026-03-04 18:31:31 +0100

    Cambio chiavi Firebase su nuovo progetto AD Next Lab Pro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-1172125722-4fbeb",
-  "appId": "1:93997751906:web:f34d376284a6232765bb3b",
-  "apiKey": "AIzaSyCydvNHxTZMivgQDGClqopoG1PiE_gZsBA",
-  "authDomain": "studio-1172125722-4fbeb.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "93997751906"
+  apiKey: "AIzaSyBDDRbD4pRoR8FTjd5EXhoRO-fAuCcx_oQ",
+  authDomain: "ad-next-lab-pro.firebaseapp.com",
+  projectId: "ad-next-lab-pro",
+  storageBucket: "ad-next-lab-pro.firebasestorage.app",
+  messagingSenderId: "240899724342",
+  appId: "1:240899724342:web:2c798bd0359f2758c6b497",
+  measurementId: "G-DXBDG3ZMYH"
 };
